b57c42850ce42445ce4feae27826f91e5f9625e3
Abenezer <abenezersoccer@gmail.com>
2022-09-20 01:11:25 -0400

    finished the map and almost donw with doReduce

--- src/mapreduce/common_reduce.go
+++ src/mapreduce/common_reduce.go
@@ -1,9 +1,18 @@
 package mapreduce
 
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"sort"
+)
+
 // doReduce does the job of a reduce worker: it reads the intermediate
 // key/value pairs (produced by the map phase) for this task, sorts the
 // intermediate key/value pairs by key, calls the user-defined reduce function
 // (reduceF) for each key, and writes the output to disk.
+type ByOrder []keyValue
+
 func doReduce(
 	jobName string, // the name of the whole MapReduce job
 	reduceTaskNumber int, // which reduce task this is
@@ -33,4 +42,50 @@ func doReduce(
 	// file.Close()
 	//
 	// Use checkError to handle errors.
+	intermediateFilesToProcess := createArrayIntermediateFileToProcess(jobName, reduceTaskNumber, nMap)
+	proccessIntermediateFile(intermediateFilesToProcess)
+}
+
+func createArrayIntermediateFileToProcess(jobName string, numberReducedTasks int, numberMapTasks int) []string {
+	index := 0
+	intermediateFilesToProcess := []string{}
+	for index < numberMapTasks {
+		fileName := reduceName(jobName, index, numberReducedTasks)
+		intermediateFilesToProcess = append(intermediateFilesToProcess, fileName)
+		index++
+	}
+	return intermediateFilesToProcess
+}
+func proccessIntermediateFile(intermediateFilesToProcess []string) {
+	bufferSortedKeyValuePairs := []KeyValue{}
+	for _, fileName := range intermediateFilesToProcess {
+		keyValuePair := readFileContent(fileName)
+		sortedKeyValuePair := sortByKey(keyValuePair)
+		bufferSortedKeyValuePairs = updateBufferedKeyValuePairNewSet(bufferSortedKeyValuePairs, sortedKeyValuePair)
+	}
+}
+func updateBufferedKeyValuePairNewSet(bufferSortedKeyValuePairs, sortedKeyValuePair []KeyValue) []KeyValue {
+	for _, keyPair := range sortedKeyValuePair {
+		bufferSortedKeyValuePairs = append(bufferSortedKeyValuePairs, keyPair)
+	}
+	return bufferSortedKeyValuePairs
+}
+func sortByKey(keyValuePair []KeyValue) []KeyValue {
+	sort.Slice(keyValuePair, func(i, j int) bool {
+		return keyValuePair[i].Key < keyValuePair[j].Key
+	})
+	return keyValuePair
+}
+
+func readFileContent(fileName string) []KeyValue {
+	content, err := os.ReadFile(fileName)
+	var keyValuePairs []KeyValue
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.Unmarshal(content, &keyValuePairs)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return keyValuePairs
 }
